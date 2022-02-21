652a13201be14316a5762bc74dde15450013e8d1
aznd <jakob.stechow@protonmail.com>
2022-02-21 17:44:17 +0100

    Use JetBrains Mono Font

--- install.sh
+++ install.sh
@@ -3,5 +3,5 @@ cp -r nvim/ ~/.config/.
 cp -r awesome/ ~/.config/.
 cp -r rofi/ ~/.config/.
 cp -r kitty/ ~/.config/.
-sudo pacman -S --needed fish git thunar rofi kitty
+sudo pacman -S --needed fish git thunar rofi kitty ttf-jetbrains-mono
 chsh -s /bin/fish
