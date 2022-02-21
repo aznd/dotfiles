986a538cc36b83e7245f48825521bebfce5ee686
aznd <jakob.stechow@protonmail.com>
2022-02-21 17:50:50 +0100

    Add yay

--- install.sh
+++ install.sh
@@ -3,5 +3,5 @@ cp -r nvim/ ~/.config/.
 cp -r awesome/ ~/.config/.
 cp -r rofi/ ~/.config/.
 cp -r kitty/ ~/.config/.
-sudo pacman -S --needed fish git thunar rofi kitty ttf-jetbrains-mono
-chsh -s /bin/fish
+sudo pacman -S --needed fish git thunar rofi kitty ttf-jetbrains-mono base-devel
+git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
