0d8bbc5b046e7b2506d381f96a10918031e57ffe
aznd <jakob.stechow@protonmail.com>
2022-02-21 17:55:31 +0100

    Use nerd patched font

--- install.sh
+++ install.sh
@@ -3,5 +3,6 @@ cp -r nvim/ ~/.config/.
 cp -r awesome/ ~/.config/.
 cp -r rofi/ ~/.config/.
 cp -r kitty/ ~/.config/.
-sudo pacman -S --needed fish git thunar rofi kitty ttf-jetbrains-mono base-devel
+sudo pacman -S --needed fish git thunar rofi kitty base-devel
 git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
+yay -S nerd-fonts-jetbrains-mono
